7ddb1fa4e12d34f47ceb50bbdcf345a5dcacfa48
erdoganabaci <erdoganabaci97@gmail.com>
2020-07-30 17:11:24 +0300

    zoom-clone ready for deploy first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+const PORT = process.env.PORT || 3001;
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+server.listen(PORT)
+console.log("Server is up!")
